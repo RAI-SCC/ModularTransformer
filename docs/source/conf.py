346b718cce1ab0f670c8b8cb84cfbfcf30d1e453
Oskar Taubert <oskar.taubert@kit.edu>
2024-06-17 13:27:06 +0200

    added automatic api documentation generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,15 +13,22 @@ author = 'Arvid Weyrauch, Pavel Zwerschke, Asena Özdemir'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinxemoji.sphinxemoji",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+autoapi_dirs = ["../../modular_transformer"]
+autoapi_python_class_content = "init"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
 html_static_path = ['_static']
